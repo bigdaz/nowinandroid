6b9a7aed3970ab653b25cc2a978ae70e42d58c99
Daz DeBoer <daz@gradle.com>
2026-06-15 14:33:22 -0600

    Connect to Solutions DV

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,19 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.gradle.develocity") version "4.4.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
+}
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+    buildScan {
+        uploadInBackground = false
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
